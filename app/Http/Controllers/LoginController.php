4496d16d26d5440da907a93239bbcfd1bf620ad5
Ezichi Ebere <tripleee@yahoo.co.uk>
2020-12-26 00:40:41 +0100

    Add controller for login.

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+/**
+ * @author Ezichi
+ */
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $input = $request->only('email', 'password');
+
+        if (!$authorized = Auth::attempt($input)) {
+            $code = 401;
+            $output = [
+                'code' => $code,
+                'message' => 'User is not authorized!'
+            ];
+
+            return response()->json($output, $code);
+        }
+
+        $token = $this->respondWithToken($authorized);
+        $code = 200;
+        $output = [
+            'code' => $code,
+            'message' => 'User logged in successfully.',
+            'token' => $token
+        ];
+
+        return response()->json($output, $code);
+    }
+}
