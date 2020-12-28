3fb43cc38ffde8d9c9e93c155c1c9e60865debe0
Ezichi Ebere <tripleee@yahoo.co.uk>
2020-12-28 01:24:31 +0100

    Added create method in controller.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use App\Models\Personal;
 
 class UserController extends Controller
 {
@@ -12,7 +15,36 @@ class UserController extends Controller
         if (count(User::all()) < 1) {
             User::factory()->count(2)->create();
         }
-        
+
         return response()->json(User::all());
+    }
+
+    public function create(Request $request)
+    {
+        $validated = $this->validate($request, [
+            'email' => 'required|unique:users',
+            'password' => 'required',
+            'first_name' => 'required',
+            'last_name' => 'required'
+        ]);
+
+        $user = User::create([
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $profile = Personal::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'user_id' => $user->id
+        ]);
+
+        $code = 200;
+        $output = [
+            'code' => $code,
+            'message' => 'User added!',
+        ];
+
+        return response()->json($output, $code);
     }
 }
