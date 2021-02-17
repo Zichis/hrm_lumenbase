2ebc0ac2b99da6000db8e9cc5fbe006b49e5f521
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-17 23:47:57 +0100

    phpcbf fixes.

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,49 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
+$router->get(
+    '/', function () use ($router) {
+        return $router->app->version();
+    }
+);
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
-
-$router->get('user/roles', ['middleware' => 'auth', 'uses' => 'UserController@roles']);
+$router->get(
+    'user/roles', [
+    'middleware' => 'auth',
+    'uses' => 'UserController@roles'
+    ]
+);
 $router->get('users/count', 'UserController@count');
-$router->group(['middleware' => ['auth', 'admin']], function () use ($router) {
-    $router->group(['prefix' => 'users'], function () use ($router) {
-        $router->get('/', 'UserController@index');
-        $router->get('{id}', 'UserController@show');
-        $router->put('{id}', 'UserController@update');
-        $router->post('/', 'UserController@create');
-        $router->delete('{id}', 'UserController@destroy');
-    });
-});
+$router->group(
+    ['middleware' => ['auth', 'admin']], function () use ($router) {
+        $router->group(
+            ['prefix' => 'users'], function () use ($router) {
+                $router->get('/', 'UserController@index');
+                $router->get('{id}', 'UserController@show');
+                $router->put('{id}', 'UserController@update');
+                $router->post('/', 'UserController@create');
+                $router->delete('{id}', 'UserController@destroy');
+            }
+        );
+    }
+);
 
-$router->get('profile', [
+$router->get(
+    'profile', [
     'middleware' => 'auth',
     'uses' => 'ProfileController@index'
-]);
+    ]
+);
 
-$router->get('current-user', [
+$router->get(
+    'current-user', [
     'middleware' => 'auth',
     'uses' => 'UserController@current'
-]);
-$router->post('onboard', [
+    ]
+);
+$router->post(
+    'onboard', [
     'uses' => 'UserController@onboard'
-]);
+    ]
+);
 $router->post('login', 'LoginController@login');
 $router->get('logout', 'UserController@logout');
