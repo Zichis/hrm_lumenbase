d31caddf8265149d5f8b7bce24a5f0915483a232
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-23 23:25:13 +0100

    Updated attendance controller.

--- app/Http/Controllers/Admin/AttendanceController.php
+++ app/Http/Controllers/Admin/AttendanceController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\User;
+use App\Models\Attendance;
 use App\Http\Controllers\Controller;
 
 class AttendanceController extends Controller
@@ -18,6 +20,20 @@ class AttendanceController extends Controller
 
     public function index()
     {
-        dd("Attendance index!");
+        $attendances = Attendance::all();
+
+        return response()->json($attendances);
+    }
+
+    public function clockIn()
+    {
+        /*$user = User::find(1);
+        $attendance = Attendance::create([
+            'date' => new \DateTime(),
+            'clock_in' => new \DateTime()
+        ]);
+        $user->attendance()->attach($attendance->id);
+
+        return response()->json(['message' => 'User clocked in']);*/
     }
 }
