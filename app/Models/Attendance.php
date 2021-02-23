8bf4ec5c30302f55cb31be5530ea06ae72f1b704
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-23 23:24:18 +0100

    Added attendance fields to fillable.

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attendance extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'date','clock_in','clock_out'
+    ];
+
     public function users()
     {
         return $this->belongsToMany(User::class);
