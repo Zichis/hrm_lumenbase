a901f1ee16bb92b8cd4c31bbdac4be86223cbbd4
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-23 23:25:52 +0100

    Corrected typo attendance to attendances.

--- database/migrations/2021_02_21_223455_create_attendance_table.php
+++ database/migrations/2021_02_21_223455_create_attendance_table.php
@@ -29,6 +29,6 @@ class CreateAttendanceTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('attendance');
+        Schema::dropIfExists('attendances');
     }
 }
