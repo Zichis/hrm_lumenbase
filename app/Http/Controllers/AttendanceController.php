ca7bb82bc399c69612ca6c89a864d7549fc15c05
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-26 23:29:20 +0100

    Created attendance controller for user.

---
+++ app/Http/Controllers/AttendanceController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AttendanceController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    public function index()
+    {
+        //
+    }
+
+    public function clockIn()
+    {
+        //
+    }
+}
