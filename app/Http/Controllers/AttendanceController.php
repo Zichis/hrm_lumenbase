4b57c8cc46f43d463db0c886e79e9671ecc7140f
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-27 23:15:43 +0100

    Added clockIn and status method.

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Attendance;
+use Illuminate\Support\Facades\Auth;
+
 class AttendanceController extends Controller
 {
 
@@ -21,6 +24,39 @@ class AttendanceController extends Controller
 
     public function clockIn()
     {
-        //
+        $today = new \DateTime();
+        $currentDate = $today->format('Y-m-d');
+        $attendance = Auth::user()->attendance()->where('date', $currentDate)->get();
+
+        if ($attendance->count() > 0) {
+            return response()->json(['msg' => 'User clocked in already!'], 403);
+        }
+
+        if ($today->format('H') < 8 || $today->format('H') > 16) {
+            return response()->json(['msg' => 'You cannot clock in before/after working hours.'], 403);
+         }
+
+        $attendance = Attendance::create([
+            'date' => new \DateTime(),
+            'clock_in' => new \DateTime()
+        ]);
+        Auth::user()->attendance()->attach($attendance->id);
+
+        return response()->json(['msg' => 'Success', 'attendances' => Auth::user()->attendance]);
+    }
+
+    public function status()
+    {
+        $today = new \DateTime();
+        $currentDate = $today->format('Y-m-d');
+        $attendance = Auth::user()->attendance()->where('date', $currentDate)->get();
+        $canClockIn = false;
+        $canClockOut = false;
+
+        if ($attendance->count() == 0 && $today->format('H') >= 8 && $today->format('H') <= 15) {
+            $canClockIn = true;
+        }
+
+        return response()->json(['msg' => 'Success', 'canClockIn' => $canClockIn]);
     }
 }
